a61a0a838841a41121cd00e58ab9aa828607c980
freyjabun <freyjabun@gmail.com>
2022-02-14 16:28:57 +0100

    schlonk

--- Assignment 2/Assignment2.fs
+++ Assignment 2/Assignment2.fs
@@ -20,3 +20,23 @@ let rec combinePair =
     | _            -> []
 
 //Exercise 2.4
+type complex = float * float
+let mkComplex x y = (x,y)
+let complexToPair (x,y) = (x,y)
+//Addition
+let (|+|) ((a:float),(b:float)) ((c:float),(d:float)) = ((a+c),(b+d))
+//Multiplication
+let (|*|) ((a:float),(b:float)) ((c:float),(d:float)) = ((a*c-b*d),(b*c+a*d))
+//Subtraction
+let (|-|) (x:complex) (y:complex) = x |+| (-(fst y), -(snd y)) 
+//Division
+exception DotnetGG
+let (|/|) (x:complex) ((a:float),(b:float)) =
+    if a = 0 || b = 0 then
+       raise DotnetGG
+    else
+    let den = a**2 + b**2
+    x |*| (a/den,(-b)/den)
+
+//Exercise 2.5
+let explode1 s:string = s.ToCharArray()
